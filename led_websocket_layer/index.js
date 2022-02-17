e4d8fdd796147c64d62f7f7a6aec6095f78b9c63
Jeffrey Yang <jeffreyhasmail@gmail.com>
2022-02-16 21:45:22 -0800

    working refresh

--- led_websocket_layer/index.js
+++ led_websocket_layer/index.js
@@ -1,16 +1,16 @@
 const WebSocket = require('ws');
-
+const redis = require('redis');
 const fs = require('fs');
 const wss = new WebSocket.Server({
   port: 8080
 });
+const publisher = redis.createClient();
+publisher.connect();
 
 wss.on('connection', ws => {
   ws.on('message', message => {
     console.log('received message ' + message);
-    fs.writeFileSync(process.env.HOME + '/myPipe', message, err => {
-      if (err) console.log(err);
-    })
+    publisher.publish('dataChannel', message);
     ws.send('server wrote');
   });
   ws.send('connected');
